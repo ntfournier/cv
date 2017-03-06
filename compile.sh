585f32d3777c21600a6b7ab9edf7b53d29daca26
Vincent Fournier <vince@ntfournier.com>
2017-03-06 13:41:51 -0500

    Add bold option to script

--- compile.sh
+++ compile.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-# Set english (default)
-sed -i 's,\\newcommand{\\inEnglish}\[1\]{},\\newcommand{\\inEnglish}\[1\]{#1},' cv.tex
-sed -i 's,\\newcommand{\\inFrench}\[1\]{#1},\\newcommand{\\inFrench}\[1\]{},' cv.tex
+# For all args will put words in bold.
+# This is usefull to highlight good experience when applying to different places.
 
-pdflatex cv.tex
-mv -f cv.pdf vincent_fournier_cv_en.pdf
+cp cv.tex cv-bold.tex
+for tech in "$@"
+do
+	sed -i 's,'"$tech"',\\textbf{'"$tech"'},' cv-bold.tex
+done
+
+# Set english (default)
+sed -i 's,\\newcommand{\\inEnglish}\[1\]{},\\newcommand{\\inEnglish}\[1\]{#1},' cv-bold.tex
+sed -i 's,\\newcommand{\\inFrench}\[1\]{#1},\\newcommand{\\inFrench}\[1\]{},' cv-bold.tex
+
+pdflatex cv-bold.tex
+mv -f cv-bold.pdf vincent_fournier_cv_en.pdf
 
 # Set french (default)
-sed -i 's,\\newcommand{\\inEnglish}\[1\]{#1},\\newcommand{\\inEnglish}\[1\]{},' cv.tex
-sed -i 's,\\newcommand{\\inFrench}\[1\]{},\\newcommand{\\inFrench}\[1\]{#1},' cv.tex
+sed -i 's,\\newcommand{\\inEnglish}\[1\]{#1},\\newcommand{\\inEnglish}\[1\]{},' cv-bold.tex
+sed -i 's,\\newcommand{\\inFrench}\[1\]{},\\newcommand{\\inFrench}\[1\]{#1},' cv-bold.tex
 
-pdflatex cv.tex
-mv -f cv.pdf vincent_fournier_cv_fr.pdf
+pdflatex cv-bold.tex
+mv -f cv-bold.pdf vincent_fournier_cv_fr.pdf
 
 # Reset to english for CVS
-sed -i 's,\\newcommand{\\inEnglish}\[1\]{},\\newcommand{\\inEnglish}\[1\]{#1},' cv.tex
-sed -i 's,\\newcommand{\\inFrench}\[1\]{#1},\\newcommand{\\inFrench}\[1\]{},' cv.tex
+sed -i 's,\\newcommand{\\inEnglish}\[1\]{},\\newcommand{\\inEnglish}\[1\]{#1},' cv-bold.tex
+sed -i 's,\\newcommand{\\inFrench}\[1\]{#1},\\newcommand{\\inFrench}\[1\]{},' cv-bold.tex
 
